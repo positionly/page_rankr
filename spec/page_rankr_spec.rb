e2a93abf371226d692940fbc872dc69b1ce5154f
Bartosz Nowak <sztosz@users.noreply.github.com>
2017-10-03 13:50:12 +0200

    Update PublicSuffix to 3.0, and don't bail when domain is invalid (#2)
    * Update PublicSuffix to 3.0
    
    * Don't raise errors for domains ob PublicSuffix lists

--- spec/page_rankr_spec.rb
+++ spec/page_rankr_spec.rb
@@ -1,35 +1,39 @@
 require 'spec_helper'
 
 describe PageRankr do
-  describe "#rank_trackers" do
-    subject{ PageRankr.rank_trackers }
+  describe '#rank_trackers' do
+    subject { PageRankr.rank_trackers }
 
-    it{ is_expected.to include(:alexa_us) }
-    it{ is_expected.to include(:alexa_global) }
-    it{ is_expected.to include(:google) }
-    it{ is_expected.to include(:moz_rank) }
+    it { is_expected.to include(:alexa_us) }
+    it { is_expected.to include(:alexa_global) }
+    it { is_expected.to include(:google) }
+    it { is_expected.to include(:moz_rank) }
   end
 
-  describe "#backlink_trackers" do
-    subject{ PageRankr.backlink_trackers }
+  describe '#backlink_trackers' do
+    subject { PageRankr.backlink_trackers }
 
-    it{ is_expected.to include(:bing) }
-    it{ is_expected.to include(:google) }
-    it{ is_expected.to include(:yahoo) }
+    it { is_expected.to include(:bing) }
+    it { is_expected.to include(:google) }
+    it { is_expected.to include(:yahoo) }
   end
 
-  describe "#index_trackers" do
-    subject{ PageRankr.index_trackers }
+  describe '#index_trackers' do
+    subject { PageRankr.index_trackers }
 
-    it{ is_expected.to include(:google) }
-    it{ is_expected.to include(:bing) }
+    it { is_expected.to include(:google) }
+    it { is_expected.to include(:bing) }
   end
 
-  describe "::Site" do
+  describe '::Site' do
     [nil, '', 'batman.thedarkknight'].each do |value|
       it "raises an exception when given #{value.inspect}" do
-        expect{PageRankr::Site(value)}.to raise_error(PageRankr::DomainInvalid)
+        expect { PageRankr::Site(value) }.to raise_error(PageRankr::DomainInvalid)
       end
+    end
+
+    it 'does not raises an exception for valid domains on Public Suffix list' do
+      expect(PageRankr::Site('auto.pl')).to be_truthy
     end
   end
 end
